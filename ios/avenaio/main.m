9adde85deb20454ca431abb904010c474e66f2b8
Miguel Zavala <miguelzavalac@gmail.com>
2021-07-15 19:25:11 -0500

    .

--- ios/avenaio/main.m
+++ ios/avenaio/main.m
@@ -3,6 +3,6 @@
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
-  @autoreleasepool {android    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  @autoreleasepool {return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
 }
